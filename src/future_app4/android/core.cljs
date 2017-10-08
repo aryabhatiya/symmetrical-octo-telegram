a72309ea1edbd05a12b6b87e5428a4ae574e8524
Aryabhatiya Algebra <aryabhatiya.algebra@gmail.com>
2017-10-08 18:04:24 +0600

    list view

--- src/future_app4/android/core.cljs
+++ src/future_app4/android/core.cljs
@@ -1,7 +1,8 @@
 (ns future-app4.android.core
   (:require-macros [rum.core :refer [defc]])
-  (:require [re-natal.support :as support]
-            [rum.core :as rum]))
+  (:require
+   [re-natal.support :as support]
+   [rum.core :as rum]))
 
 (set! js/window.React (js/require "react"))
 (def ReactNative (js/require "react-native"))
@@ -15,6 +16,9 @@
 (def text  (partial create-element (.-Text ReactNative)))
 (def image (partial create-element (.-Image ReactNative)))
 (def touchable-highlight (partial create-element (.-TouchableHighlight ReactNative)))
+(def list2  (partial create-element (.-List  ReactNative)))
+(def flat-list  (partial create-element (.-FlatList  ReactNative)))
+
 
 (def logo-img (js/require "./images/cljs.png"))
 
@@ -41,7 +45,46 @@
                                 (swap! app-state assoc :input text))})
 
         (text {:style {:fontSize 30 :fontWeight "100" :marginBottom 20 :textAlign "center" }}
-              (:input (rum/react state)))))
+              (:input (rum/react state)))
+        (flat-list {:data (clj->js [{:key "1"
+                                     :item "hello1"}
+                                    {:key "2"
+                                     :item "hello2"}
+                                    {:key "3"
+                                     :item "hello3"}
+                                    {:key "4"
+                                     :item "hello4"}
+                                    {:key "5"
+                                     :item "hello5"}
+                                    {:key "7"
+                                     :item "hello6"}
+                                    {:key "8"
+                                     :item "hello7"}
+                                    {:key "9"
+                                     :item "hello9"}
+                                    {:key "10"
+                                     :item "hello10"}
+                                    {:key "11"
+                                     :item "hello11"}
+                                    {:key "12"
+                                     :item "hello12"}
+                                    {:key "13"
+                                     :item "hello13"}
+                                    {:key "15"
+                                     :item "hello14"}
+                                    {:key "16"
+                                     :item "hello15"}
+                                    {:key "18"
+                                     :item "hello16"}
+                                    {:key "19"
+                                     :item "hello17"}
+                                    ])
+                    :renderItem (fn [d]
+                                  (text {:style {:fontSize 30
+                                                 :fontWeight "100"
+                                                 :marginBottom 20
+                                                 :textAlign "center" }}
+                                        (.-item (aget d "item"))))})))
 
 (defonce root-component-factory (support/make-root-component-factory))
 
